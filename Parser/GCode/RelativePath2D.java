501bd21da6c9004c37544a7826679c6028cba9ca
justinT21 <jjturcot@gmail.com>
2023-03-21 10:44:44 -0700

    More progress on GCode Parsing, added Point3D to RelativePath2D to be able to have a z height(does nothing to the actual path)

--- Parser/GCode/RelativePath2D.java
+++ Parser/GCode/RelativePath2D.java
@@ -1,6 +1,7 @@
 package Parser.GCode;
 
 import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
 
 /**
  * Defines a geometric path with relative coordinates and arcs possible
@@ -9,7 +10,20 @@ import java.awt.geom.Path2D;
  * @see Path2D.Double
  */
 public class RelativePath2D extends Path2D.Double {
+    public Point3D getCurrentPoint3D() {
+        return new Point3D(getCurrentPoint().getX(), getCurrentPoint().getY(), z);
+    }
+
+    private double z;
     private double xP, yP;
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public void setZRelative(double deltaZ) {
+        z += deltaZ;
+    }
 
     /**
      * Adds an arc segment, defined by 2 points, by drawing an arc that intersects
@@ -22,7 +36,7 @@ public class RelativePath2D extends Path2D.Double {
      * @param direction -1 = clockwise, 0 = nothing, 1 = counterclockwise, throws
      */
     public void arcTo(double x1, double y1, double x2, double y2, int direction) {
-        //TODO fix arc to Bezier conversion(only works for <90 degrees)
+        // TODO fix arc to Bezier conversion(only works for <90 degrees)
         if (direction < -1 || direction > 1) {
             throw new IllegalArgumentException("Direction: " + direction + " is not in the range -1, 1");
         }
@@ -33,11 +47,11 @@ public class RelativePath2D extends Path2D.Double {
         double q1 = ax * ax + ay * ay;
         double q2 = q1 + ax * bx + ay * by;
         double k2 = (4.0 / 3) * (Math.sqrt(2 * q1 * q2) - q2) / (ax * by - ay * bx);
-        curveTo(x1+ax-k2*ay, y1+ay+k2*ax, x1+bx+k2*by, y1+by-k2*bx, x2, y2);
+        curveTo(x1 + ax - k2 * ay, y1 + ay + k2 * ax, x1 + bx + k2 * by, y1 + by - k2 * bx, x2, y2);
     }
 
-    public void arcToRelative(double x1, double y1, double x2, double y2, int direction){
-        arcTo(x1+xP, y1+xP, x2+xP, y2+yP, direction);
+    public void arcToRelative(double x1, double y1, double x2, double y2, int direction) {
+        arcTo(x1 + xP, y1 + xP, x2 + xP, y2 + yP, direction);
         setRelative(x2, y2);
     }
 
@@ -118,5 +132,35 @@ public class RelativePath2D extends Path2D.Double {
     public void curveToRelative(double x1, double y1, double x2, double y2, double x3, double y3) {
         curveTo(x1 + xP, y1 + yP, x2 + xP, y2 + yP, x3 + xP, y3 + yP);
         setRelative(x3, y3);
+    }
+}
+
+class Point3D extends Point2D.Double {
+    private double z;
+
+    public Point3D(double x, double y, double z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setLocation(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    /**
+     * Returns a {@code String} that represents the value
+     * of this {@code Point3D}.
+     * 
+     * @return a string representation of this {@code Point3D}.
+     */
+    public String toString() {
+        return "Point3D[" + x + ", " + y + ", " + z + "]";
     }
 }
