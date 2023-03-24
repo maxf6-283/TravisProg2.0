c9110ab36fd1656f890c63912eddf8ed676268e1
justinT21 <jjturcot@gmail.com>
2023-03-24 14:35:14 -0700

    getting rid of some unneeded imports

--- Display/NewSheetPrompt.java
+++ Display/NewSheetPrompt.java
@@ -8,14 +8,12 @@ import SheetHandler.SheetThickness;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 public class NewSheetPrompt extends JFrame implements ActionListener{
     private JButton enterSheetInfo;
