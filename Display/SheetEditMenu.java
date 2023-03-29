69bff7c77189bac626c281132663659d97124e25
justinT21 <jjturcot@gmail.com>
2023-03-29 12:31:21 -0700

    layout added

--- Display/SheetEditMenu.java
+++ Display/SheetEditMenu.java
@@ -3,21 +3,79 @@ package Display;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class SheetEditMenu extends JPanel {
-    public SheetEditMenu(){
-        setLayout(null);
+public class SheetEditMenu extends JPanel implements ActionListener {
+    private JButton emit;
+
+    public SheetEditMenu() {
+        setLayout(new GridBagLayout());
         setBounds(0, 0, 300, 800);
+        GridBagConstraints c = new GridBagConstraints();
+
+        emit = new JButton("Emit Gcode");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridy = 0;
+        add(emit, c);
+        JPanel pane = this;
+        JButton button;
+
+        button = new JButton("Button 2");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 1;
+        c.gridy = 0;
+        pane.add(button, c);
+
+        button = new JButton("Button 3");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.gridx = 2;
+        c.gridy = 0;
+        pane.add(button, c);
+
+        button = new JButton("Long-Named Button 4");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 40; // make this component tall
+        c.weightx = 0.0;
+        c.gridwidth = 3;
+        c.gridx = 0;
+        c.gridy = 1;
+        pane.add(button, c);
+
+        button = new JButton("5");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 0; // reset to default
+        c.weighty = 1.0; // request any extra vertical space
+        c.anchor = GridBagConstraints.PAGE_END; // bottom of space
+        c.insets = new Insets(10, 0, 0, 0); // top padding
+        c.gridx = 1; // aligned with button 2
+        c.gridwidth = 2; // 2 columns wide
+        c.gridy = 2; // third row
+        pane.add(button, c);
+
     }
 
     @Override
-    public Dimension getPreferredSize(){
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
         return new Dimension(300, 800);
     }
 
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, getWidth(), getHeight());
