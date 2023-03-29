ee0cb7cda5414189efbc9f47841d0148287c4b83
ThePersonOfStuff <maxf6.283@gmail.com>
2023-03-29 14:59:09 -0700

    Added icon

--- Runner.java
+++ Runner.java
@@ -1,4 +1,10 @@
 import Display.*;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -18,7 +24,13 @@ public class Runner {
             // handle exception
         }
         Screen screen = new Screen();
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("JustinAndMaxProg");
+        try {
+            BufferedImage img = ImageIO.read(new File("Display/971 Icon.png"));
+            frame.setIconImage(img);
+        } catch(IOException e) {
+            System.err.println("No icon image found");
+        }
 
         frame.add(screen);
 
