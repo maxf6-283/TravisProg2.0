94f835cc5f75d2907c6be93423b1bd0b0b778dbd
justinT21 <jjturcot@gmail.com>
2023-03-29 14:51:45 -0700

    moved to nested class

--- Display/Screen.java
+++ Display/Screen.java
@@ -129,115 +129,7 @@ public class Screen extends JPanel
         newSheetPrompt.setVisible(false);
         newSheetPrompt.setAlwaysOnTop(true);
 
-        editMenu = new JPanel() {
-            {
-                setLayout(new GridBagLayout());
-                setBounds(0, 0, 300, 800);
-
-                GridBagConstraints c = new GridBagConstraints();
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.anchor = GridBagConstraints.FIRST_LINE_START;
-                c.insets = new Insets(10, 10, 10, 10);
-                c.gridx = 0;
-                c.gridy = 0;
-                c.weightx = 0.5;
-                c.ipady = 40;
-                c.gridwidth = 3;
-                add(returnToHome, c);
-
-                sheetName = new JLabel("Editing Sheet: ");
-                sheetName.setForeground(Color.WHITE);
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.weightx = 0.5;
-                c.gridx = 0;
-                c.gridy = 1;
-                c.ipady = 20;
-                add(sheetName, c);
-
-                addHole = new JButton("Add Hole");
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.weightx = 0.5;
-                c.gridx = 0;
-                c.gridy = 2;
-                c.gridwidth = 1;
-                c.ipady = 20;
-                add(addHole, c);
-
-                addItem = new JButton("Add Item");
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 1;
-                c.gridy = 2;
-                add(addItem, c);
-
-                del = new JButton("Del Select");
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 2;
-                c.gridy = 2;
-                add(del, c);
-
-                reScan = new JButton("Rescan parts_library");
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 0;
-                c.gridwidth = 2;
-                c.gridy = 3;
-                add(reScan, c);
-
-                emit = new JButton("Emit GCode");
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 0;
-                c.gridwidth = 1;
-                c.gridy = 4;
-                add(emit, c);
-
-                save = new JButton("Save");
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 1;
-                c.gridy = 4;
-                add(save, c);
-
-                addCut = new JButton("Add Cut");
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 2;
-                c.gridy = 4;
-                add(addCut, c);
-
-                cutName = new JLabel("Current Cut: ");
-                cutName.setForeground(Color.WHITE);
-                c.fill = GridBagConstraints.HORIZONTAL;
-                c.gridx = 0;
-                c.gridwidth = 3;
-                c.gridy = 5;
-                add(cutName, c);
-
-                // bottom buffer
-                c.gridx = 0;
-                c.gridy = 6;
-                c.weighty = 1;
-                add(new JLabel(), c);
-
-                if (selectedSheet != null) {
-                    sheetName.setText("Editing Sheet: " + selectedSheet.getSheetFile().getName());
-                    cutName.setText("Current Cut: " + selectedSheet.getActiveCutFile().getName());
-                }
-
-            }
-
-            @Override
-            public Dimension getPreferredSize() {
-                return new Dimension(300, 800);
-            }
-
-            @Override
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                if (selectedSheet != null) {
-                    sheetName.setText("Editing Sheet: " + selectedSheet.getSheetFile().getName());
-                    cutName.setText("Current Cut: " + selectedSheet.getActiveCutFile().getName());
-                }
-                g.setColor(Color.BLACK);
-                g.fillRect(0, 0, getWidth(), getHeight());
-            }
-        };
+        editMenu = new SheetEditMenu();
         add(editMenu);
         editMenu.setVisible(false);
 
@@ -615,6 +507,114 @@ public class Screen extends JPanel
             rotatingPart = false;
             rotationPoint = null;
             repaint();
+        }
+    }
+    public class SheetEditMenu extends JPanel{
+        public SheetEditMenu(){
+            setLayout(new GridBagLayout());
+            setBounds(0, 0, 300, 800);
+
+            GridBagConstraints c = new GridBagConstraints();
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.anchor = GridBagConstraints.FIRST_LINE_START;
+            c.insets = new Insets(10, 10, 10, 10);
+            c.gridx = 0;
+            c.gridy = 0;
+            c.weightx = 0.5;
+            c.ipady = 40;
+            c.gridwidth = 3;
+            add(returnToHome, c);
+
+            sheetName = new JLabel("Editing Sheet: ");
+            sheetName.setForeground(Color.WHITE);
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.weightx = 0.5;
+            c.gridx = 0;
+            c.gridy = 1;
+            c.ipady = 20;
+            add(sheetName, c);
+
+            addHole = new JButton("Add Hole");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.weightx = 0.5;
+            c.gridx = 0;
+            c.gridy = 2;
+            c.gridwidth = 1;
+            c.ipady = 20;
+            add(addHole, c);
+
+            addItem = new JButton("Add Item");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 1;
+            c.gridy = 2;
+            add(addItem, c);
+
+            del = new JButton("Del Select");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 2;
+            c.gridy = 2;
+            add(del, c);
+
+            reScan = new JButton("Rescan parts_library");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridwidth = 2;
+            c.gridy = 3;
+            add(reScan, c);
+
+            emit = new JButton("Emit GCode");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridwidth = 1;
+            c.gridy = 4;
+            add(emit, c);
+
+            save = new JButton("Save");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 1;
+            c.gridy = 4;
+            add(save, c);
+
+            addCut = new JButton("Add Cut");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 2;
+            c.gridy = 4;
+            add(addCut, c);
+
+            cutName = new JLabel("Current Cut: ");
+            cutName.setForeground(Color.WHITE);
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridwidth = 3;
+            c.gridy = 5;
+            add(cutName, c);
+
+            // bottom buffer
+            c.gridx = 0;
+            c.gridy = 6;
+            c.weighty = 1;
+            add(new JLabel(), c);
+
+            if (selectedSheet != null) {
+                sheetName.setText("Editing Sheet: " + selectedSheet.getSheetFile().getName());
+                cutName.setText("Current Cut: " + selectedSheet.getActiveCutFile().getName());
+            }
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(300, 800);
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (selectedSheet != null) {
+                sheetName.setText("Editing Sheet: " + selectedSheet.getSheetFile().getName());
+                cutName.setText("Current Cut: " + selectedSheet.getActiveCutFile().getName());
+            }
+            g.setColor(Color.BLACK);
+            g.fillRect(0, 0, getWidth(), getHeight());
         }
     }
 }
