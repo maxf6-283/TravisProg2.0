eef9f87f3e9d6f460ca1c7d78dcbd218933a8d48
justinT21 <jjturcot@gmail.com>
2023-04-04 19:26:54 -0700

    fixing measure button bug

--- Display/Screen.java
+++ Display/Screen.java
@@ -239,6 +239,7 @@ public class Screen extends JPanel
         g.fillRect(0, 0, getWidth(), getHeight());
         // set all menu JPanels not visible
         editMenu.setVisible(false);
+        measure.setForeground(menuState == SheetMenuState.MEASURE ? Color.LIGHT_GRAY : null);
         switch (state) {
             case SHEET_SELECT -> {
                 g.drawImage(img, (getWidth() - 800) / 2, (getHeight() - 800) / 2, null);
@@ -774,7 +775,7 @@ public class Screen extends JPanel
         }
     }
 
-    private enum SheetMenuState {
+    public enum SheetMenuState {
         NULL, HOME, MEASURE, CUT_SELECT, GCODE_SELECT, EMIT_SELECT, ADD_ITEM, ADD_HOLE
     }
 }
