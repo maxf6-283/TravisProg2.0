a689d9ee975cda12cf4adfd574c3fbeeb4223453
justinT21 <jjturcot@gmail.com>
2023-04-14 09:30:53 -0700

    outline for holes to account for head size

--- SheetHandler/Hole.java
+++ SheetHandler/Hole.java
@@ -1,9 +1,50 @@
 package SheetHandler;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
 import java.io.File;
 
+import Parser.GCode.NGCDocument;
+
 public class Hole extends Part {
+    public static final double HEAD_SIZE = 0.4;
     public Hole(File holeFile, double x, double y, double rot) {
         super(holeFile, x, y, rot);
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        AffineTransform prevTransform = g2d.getTransform();
+        g2d.translate(getX(), -getY());
+        Color prevColor = g.getColor();
+        g2d.rotate(-getRot());
+        if (getSelected() == true) {
+            g.setColor(Color.RED);
+        } /*
+           * else {
+           * g.setColor(Color.ORANGE);
+           * }
+           */
+        Stroke currentStrok = g2d.getStroke();
+        // ((Graphics2D)g).draw(new Ellipse2D.Double(getX()-0.5,-sheetY-1,1,2));
+
+        for (NGCDocument activeNgcDoc : getNgcDocuments()) {
+            g2d.setStroke(
+                    new BasicStroke((float) activeNgcDoc.getToolOffset(), BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND,
+                            100000));
+            activeNgcDoc.getRelativePath2Ds().stream().forEach(e -> g2d.draw(e));
+        }
+
+        g2d.setStroke(currentStrok);
+        g2d.setColor(Color.ORANGE);
+        g2d.draw(new Ellipse2D.Double(-HEAD_SIZE/2, -HEAD_SIZE/2, HEAD_SIZE, HEAD_SIZE));
+        g2d.setColor(prevColor);
+        g2d.setTransform(prevTransform);
     }
 }
